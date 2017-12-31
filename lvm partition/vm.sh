d71b8d694365f8506f8cd77348c089fc303a613d
Bart Kus <b4rtmspam@gmail.com>
2017-12-31 16:34:02 +0100

    add an usb example and clean partition examples

--- lvm partition/vm.sh
+++ lvm partition/vm.sh
@@ -2,6 +2,7 @@
 #
 # ToDo
 # 1. change drive file to existing lvm volume.
+img= /dev/mapper/group-volume &&
 
 qemu-system-x86_64 -enable-kvm -M pc-q35-2.10 -m 2000 \
     -cpu host \
@@ -11,6 +12,6 @@ qemu-system-x86_64 -enable-kvm -M pc-q35-2.10 -m 2000 \
     -rtc base=localtime \
     -soundhw hda \
     -usbdevice tablet \
-    -drive file=/dev/mapper/group-volume,if=virtio,id=disk1,format=raw,cache=none,media=disk \
+    -drive file=$img,if=virtio,id=disk1,format=raw,cache=none,media=disk \
     -spice port=5900,disable-ticketing
 
---
+++ usb device/vm.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+#
+# ToDo
+# 1. change img to existing file image.
+# 2. check usb device id (e.g. with lsusb) and set it as usbId
+
+
+img= /home/$USER/vm.img &&
+usbId= 1d1b:0001 &&
+
+qemu-system-x86_64 -enable-kvm -M pc-q35-2.10 -m 2000 \
+    -cpu host \
+    -smp 1,sockets=1,cores=1,threads=1 \
+    -vga qxl \
+    -net none \
+    -rtc base=localtime \
+    -soundhw hda \
+    -usbdevice tablet \
+    -drive file=$img,if=virtio,id=disk1,format=raw,cache=none,media=disk \
+    -usb -usbdevice host:$usbId \
+    -spice port=5900,disable-ticketing
+
