5a25e261d5a385044d1eaa7eabf1820d1e1dc933
Bart Kus <b4rtmspam@gmail.com>
2017-12-31 16:14:59 +0100

    add partition example

---
+++ partition/vm.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+#
+# ToDo
+# 1. change drive file to existing partition e.g. /dev/sdb1.
+
+qemu-system-x86_64 -enable-kvm -M pc-q35-2.10 -m 2000 \
+    -cpu host \
+    -smp 1,sockets=1,cores=1,threads=1 \
+    -vga qxl \
+    -net none \
+    -rtc base=localtime \
+    -soundhw hda \
+    -usbdevice tablet \
+    -drive file=/dev/sdzX,if=virtio,id=disk1,format=raw,cache=none,media=disk \
+    -spice port=5900,disable-ticketing
+
